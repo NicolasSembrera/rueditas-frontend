e7b4c5cfa761b79e5cf6a5fe6382ac38dde94c06
NicolasSembrera <nsembrera@gmail.com>
2024-09-27 09:16:36 -0500

    Ultimos ajustes

--- src/main/java/pe/edu/cibertec/rueditas_frontend/controller/BusquedaController.java
+++ src/main/java/pe/edu/cibertec/rueditas_frontend/controller/BusquedaController.java
@@ -5,7 +5,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
+import pe.edu.cibertec.rueditas_frontend.dto.BusquedaResponseDTO;
 import pe.edu.cibertec.rueditas_frontend.viewmodel.BusquedaModel;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Controller
 @RequestMapping("/matricula")
@@ -19,16 +23,47 @@ public class BusquedaController {
 
         BusquedaModel busquedaModel = new BusquedaModel("00","","","","","","");
         model.addAttribute("busquedaModel", busquedaModel);
-    return "busqueda";
+        return "inicio";
     }
 
     @PostMapping("/buscar")
-    public String buscarVehiculo(@RequestParam String matricula, Model model){
+    public String buscarVehiculo(@RequestParam String nroMatricula, Model model){
 
-        BusquedaModel busquedaModel = new BusquedaModel("00","","","","","","");
-        model.addAttribute("busquedaModel", busquedaModel);
+        if (nroMatricula.matches("^[A-Za-z0-9]{3}-[A-Za-z0-9]{3}$")){
 
-        return "";
+            try {
+                String URL = "http://localhost:8081/autos/buscar?nroplaca=" + nroMatricula;
+                Map<String, String> parametros = new HashMap<>();
+                parametros.put("nroplaca", nroMatricula);
+                BusquedaResponseDTO busquedaResponseDTO = restTemplate.postForObject(URL, null, BusquedaResponseDTO.class, parametros);
+
+                if (busquedaResponseDTO.codigo().equals("00")){
+                    BusquedaModel busquedaModel = new BusquedaModel("00","",busquedaResponseDTO.marca(),busquedaResponseDTO.modelo(),busquedaResponseDTO.nasientos(),busquedaResponseDTO.precio(),busquedaResponseDTO.color());
+                    model.addAttribute("busquedaModel", busquedaModel);
+
+                    return "busqueda";
+                }
+
+                BusquedaModel busquedaModel = new BusquedaModel("01","No se encontro vehiculo con la placa digitada", "", "", "", "", "");
+                model.addAttribute("busquedaModel", busquedaModel);
+
+                return "inicio";
+
+            }catch (Exception e){
+
+                BusquedaModel busquedaModel = new BusquedaModel("99", "Error : "+e.getMessage(), "", "", "", "", "");
+                model.addAttribute("busquedaModel", busquedaModel);
+
+                return "inicio";
+            }
+
+        }else{
+            BusquedaModel busquedaModel = new BusquedaModel("01", "Debe digitar una placa valida", "", "", "", "", "");
+            model.addAttribute("busquedaModel", busquedaModel);
+
+            return "inicio";
+        }
+
     }
 
 
---
+++ src/main/java/pe/edu/cibertec/rueditas_frontend/dto/BusquedaResponseDTO.java
@@ -0,0 +1,4 @@
+package pe.edu.cibertec.rueditas_frontend.dto;
+
+public record BusquedaResponseDTO(String codigo, String mensaje, String marca, String modelo, String nasientos, String precio, String color) {
+}
